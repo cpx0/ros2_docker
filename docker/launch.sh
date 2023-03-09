a3ed64b2665daa5c95f0394bdb6a6e297b37eecb
cpx <74306862+cpx0@users.noreply.github.com>
2023-03-09 10:26:51 +0900

    [fix] update docker/launch.sh

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# TODO: User should be refactored instead of hard coded olympus
+# TODO: User should be refactored instead of hard coded cpx
 
-USER_NAME=olympus
+USER_NAME=cpx
 CMDNAME=`basename $0`
 
 read -t 10 -p "input for selecting ROS Distro (default: humble): " SELECT_ROS_DISTRO
@@ -17,11 +17,11 @@ function main() {
     echo "Usage: $CMDNAME container_name host_port image_tag"
 
     if [ ${SELECTED_ROS_DISTRO} = "humble" ]; then
-        container=${1:-"mycobot-humble"}
+        container=${1:-"ros-humble"}
         host_port=${2:-"9280"}
         tag=${3:-"22.04-cu11.7-humble"}
     elif [ ${SELECTED_ROS_DISTRO} = "galactic" ]; then
-        container=${1:-"mycobot-galactic"}
+        container=${1:-"ros-galactic"}
         host_port=${2:-"9280"}
         tag=${3:-"20.04-cu11.3-galactic"}
     else
@@ -29,14 +29,16 @@ function main() {
     fi
 
     echo "docker container name: $container"
-    echo "docker host expose port: $host_port"
+    echo "docker host:container expose port: $host_port added by user"
     echo "docker image 'ros2-cuda:$tag'"
 
     # echo "JupyterLab open at http://127.0.0.1:$host_port "
 
     docker run --gpus all -ti --init --rm \
         --hostname $(hostname) --name $container \
-        -p $host_port:2980 --shm-size=16384m \
+        --user $(id -u):$(id -g) \
+        -p 5005:5005 -p 10000:10000 \
+        -p $host_port:$host_port --shm-size=16384m \
         -v $(pwd)/src:/home/$USER_NAME/workspace/src \
         -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
         ros2-cuda:$tag
@@ -46,4 +48,3 @@ function main() {
 }
 
 main "$@"
-
